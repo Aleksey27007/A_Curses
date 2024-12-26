7ea77ea0016c382dc6ca8ff6e971f6b0c7b84e87
Aleksey27007 <alekseyvaruj@gmail.com>
2024-12-26 22:00:14 +0300

    finished ex2

--- lesson1/src/main/java/com/aston/lesson1hw/DistinctNumbers.java
+++ lesson1/src/main/java/com/aston/lesson1hw/DistinctNumbers.java
@@ -1,4 +1,22 @@
 package com.aston.lesson1hw;
 
+import java.util.Arrays;
+
 public class DistinctNumbers {
+    // int[] ints = {1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9};
+    // Удалить дубликаты из массива и вывести в консоль
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9};
+
+        getDistinctNumbers(ints);
+    }
+
+    public static void getDistinctNumbers(int[] ints) {
+        int[] cleanedArray = Arrays.stream(ints).distinct().toArray();
+
+        for (int elem : cleanedArray) {
+            System.out.print(elem + " ");
+        }
+    }
 }
