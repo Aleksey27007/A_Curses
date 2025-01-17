f89ff4a7bdca10029fe5602883cee4bac00df853
Aleksey27007 <alekseyvaruj@gmail.com>
2025-01-17 18:54:38 +0300

    added 2 readme for 3, 4 lessons

--- lesson2/src/main/java/com/aston/lesson2hw/MyArrayList.java
+++ lesson2/src/main/java/com/aston/lesson2hw/MyArrayList.java
@@ -4,17 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
+/**trimToSize() для удаления лишних пустых ячеек*/
 
+/**
+ * Реализуем интерфейс MyList для нашего MyArrayList, чтобы реализовать методы,
+ * находящиеся в MyList.*/
 public class MyArrayList<T> implements MyList<T>{
+    /**
+     * DEFAULT_CAPACITY стандартная ёмкость для созданного нами объекта MyArrayList - 10 */
     private static final int DEFAULT_CAPACITY = 10;
+    /** Object[] elements массив элемментов, нужен для складывания в него элементы, тип Object т.к.
+     * все типы наследуются от него и мы в таком случаем можем запихнуть сюда любой элемент с любым типом данных */
     private Object[] elements;
+    /** size количество элементов списка */
     private int size;
 
+    /** Первый конструктор класса, ничего не принимает,
+     * создает новый массив со стандартной емкостью DEFAULT_CAPACITY ,
+     * инициализирует size = 0 */
     public MyArrayList() {
         this.elements = new Object[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
+    /** Второй конструктор класса, принимает значение ёмкости списка,
+     * 1 условие если ёмкость > 0, то создаёт новый список типа Object, с размером = capacity;
+     * 2 условие если ёмкость == 0, то список остаётся не инициализорованным
+     * 3 в остальных случаях недопстимое значение capacity
+     * это нужно для проверки на не допустимые значения на вход, т.к. в таком случае могут быть ошибки с недопустимым
+     * параметром */
     public MyArrayList(int capacity) {
         if (capacity > 0) {
             this.elements = new Object[capacity];
@@ -25,15 +43,24 @@ public class MyArrayList<T> implements MyList<T>{
                     capacity);
         }
     }
-
+    /** Третий конструктор класса, нужен в случаях, если у нас есть колекция и нам нужно копировать элементы из
+     * сторонней коллекции в нашу MyArrayList, в таком случае создастся наша коллекция с данными из другой
+     * Запись вида "? extends ..." или "? super ..." — называется wildcard или символом подстановки, с верхней границей
+     * (extends) или с нижней границей (super). Collection<? extends T> может содержать объекты, класс которых является
+     * Collection или наследуется от Collection */
     public MyArrayList(Collection<? extends T> collection) {
+        /** Конвертируем коллецию из параметра в массив*/
         Object[] array = collection.toArray();
+        /** Если массив получлся не пустой */
         if ((size = array.length) != 0) {
+            /** Если классы и размер совпадают, то можно просто копировать таким образом элементы */
             if (collection.getClass() == ArrayList.class) {
                 elements = array;
             } else {
+            /** В противном случае инициализируется новый массив типа Object с нужным нам размером size */
                 elements = Arrays.copyOf(array, size, Object[].class);
             }
+        /** Если пустой, то и значений в нём не должно быть */
         } else {
             elements = null;
         }
@@ -47,7 +74,7 @@ public class MyArrayList<T> implements MyList<T>{
     // проверка ёмкости и обеспечение расширения массива на 30 проц за каждый вызов метода при условии заполненности массива
     private void ensureCapacity() {
         if (size == elements.length) {
-            int newCapacity = elements.length + (int) (elements.length * 0.3);
+            int newCapacity = elements.length + (int) (elements.length * 0.5);
             elements = Arrays.copyOf(elements, newCapacity);
         }
     }
@@ -129,9 +156,5 @@ public class MyArrayList<T> implements MyList<T>{
 
     public int size() {
         return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
     }
 }
